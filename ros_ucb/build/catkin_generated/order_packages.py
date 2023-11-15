4637b3dda083c4ccaf4713cdbfd91d9eac58ed2c
Luis-Lanza08 <luis.lanza@ucb.edu.bo>
2023-11-15 16:49:29 -0400

    eliminando build y devel

--- ros_ucb/build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/ros_ucb/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/ros_ucb/devel;/opt/ros/noetic'.split(';') if '/home/ros_ucb/devel;/opt/ros/noetic' != '' else []
--- ros_ucb/devel/share/beginner_tutorials/cmake/beginner_tutorials-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(beginner_tutorials_MESSAGE_FILES "/home/ros_ucb/src/beginner_tutorials/msg/Num.msg;/home/ros_ucb/src/beginner_tutorials/msg/MyCustomMessageType.msg")
-set(beginner_tutorials_SERVICE_FILES "/home/ros_ucb/src/beginner_tutorials/srv/AddTwoInts.srv")
